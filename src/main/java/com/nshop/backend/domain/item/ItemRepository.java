1445b53eaaeba7fda83833e5fa7773c44d9cf23f
whwjdan <whwjdan@naver.com>
2024-03-07 01:01:50 +0900

    test3

--- src/main/java/com/nshop/backend/domain/item/ItemRepository.java
+++ src/main/java/com/nshop/backend/domain/item/ItemRepository.java
@@ -2,6 +2,7 @@ package com.nshop.backend.domain.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 }
