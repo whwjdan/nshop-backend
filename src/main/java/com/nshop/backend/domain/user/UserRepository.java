4b3195e6b02a45be2f129092da1c039cc9826f70
whwjdan <whwjdan@naver.com>
2024-03-13 20:01:33 +0900

    feat: 로그인 기능 추가

--- src/main/java/com/nshop/backend/domain/user/UserRepository.java
+++ src/main/java/com/nshop/backend/domain/user/UserRepository.java
@@ -2,5 +2,9 @@ package com.nshop.backend.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsernameAndPassword(String username, String password);
 }
